fbfc1d79964eee5e61bc4bd8fcda47bce12499a0
prakal <kalvapal@ualberta.ca>
2015-03-25 15:57:08 -0700

    finished queue in functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,28 @@ var Queue = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {},
+      startQ = 0,
+      endQ = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[endQ] = value;
+    endQ++;
   };
 
   someInstance.dequeue = function(){
+    if ((endQ - startQ) > 0) {
+      var temp = storage[startQ];
+      delete storage[startQ];
+      startQ++;
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return endQ - startQ;
   };
 
   return someInstance;
