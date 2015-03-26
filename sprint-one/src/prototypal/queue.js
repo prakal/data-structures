f96d235bd0c05f6413b474f9d7cea92038b7ad89
prakal <kalvapal@ualberta.ca>
2015-03-25 21:16:11 -0700

    mostly finished queue of prototypal instantiation. some prototype tests not passing, but beyond our scope.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
-var Queue = function() {
+var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(Queue.prototype);
+  // instance.total = 0;
+
+  instance.startQ = 0;
+  instance.endQ = 0;
+  return instance;
+
 };
 
+// var queueMethods = Object.create(Queue.prototype);
 var queueMethods = {};
+Queue.prototype.size = function(){
+  return this.endQ - this.startQ;
+};
+
+Queue.prototype.enqueue = function(value){
+  this[this.endQ] = value;
+  this.endQ++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.endQ - this.startQ > 0){
+    var temp = this[this.startQ];
+    delete this[this.startQ];
+    this.startQ++;
+  }
+  return temp;
+};
+
 
 
