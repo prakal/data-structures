6a70dd5d11225284da570b9fab046349413fe962
prakal <kalvapal@ualberta.ca>
2015-03-26 16:05:30 -0700

    passed all tests for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,44 @@
 var LinkedList = function(){
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = {};
+  list.tail = {};
+  list.positionTail = 0;
+  list.positionHead = 0;
+  list.element = {};
+  list.head.value = null;
+
+  list.resetHead = function(){
+    var keys = Object.keys(list.element);
+    list.head.value = list.element[keys[0]] || null;
+  };
 
   list.addToTail = function(value){
+    list.tail.value = value;
+    list.element[list.positionTail] = value;
+    // base case for list.element with one element
+    list.resetHead();
+    list.positionTail++;
   };
 
   list.removeHead = function(){
+    // console.log('before delete, list looks like',list.element);
+    var temp = list.element[list.positionHead];
+    delete list.element[list.positionHead];
+    list.resetHead();
+    // console.log('after delete, list looks like', list.element);
+    list.positionHead++;
+    // console.log('head',list.head);
+    return temp;
   };
 
+
   list.contains = function(target){
+    for (var key in list.element) {
+      if (list.element[key] === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
