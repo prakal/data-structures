3efcb1934eecc33e57654381b70776517c537feb
prakal <kalvapal@ualberta.ca>
2015-03-25 21:21:08 -0700

    finished stack in prototypal instantiation. some tests are not passing but they are beyond our scope.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(Stack.prototype);
+  instance.total = 0;
+  return instance;
 };
 
 var stackMethods = {};
 
+Stack.prototype.size = function(){
+  return this.total;
+};
 
+Stack.prototype.push = function(value){
+  this.total++;
+  this[this.total] = value;
+};
+Stack.prototype.pop = function(){
+  if (this.total > 0){
+    var temp = this[this.total];
+    delete this[this.total];
+    this.total--;
+  }
+  return temp;
+};
