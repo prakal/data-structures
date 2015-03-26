28e1664a4e4e962658937df35d4aaa0d623ecdda
prakal <kalvapal@ualberta.ca>
2015-03-25 20:29:46 -0700

    modified small sp mistake in spec. finished stack and queue in functional shared instantiation pattern.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,37 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  // instance.total = 0;
+
+  instance.startQ = 0;
+  instance.endQ = 0;
+
+  instance.size = queueMethods.size;
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  return instance;
+
 };
 
 var queueMethods = {};
 
+queueMethods.size = function(){
+  return this.endQ - this.startQ;
+};
+
+queueMethods.enqueue = function(value){
+  this[this.endQ] = value;
+  this.endQ++;
+};
+
+queueMethods.dequeue = function(){
+  if (this.endQ - this.startQ > 0){
+    var temp = this[this.startQ];
+    delete this[this.startQ];
+    this.startQ++;
+  }
+  return temp;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
+//functional shared uses objects to store information
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance={};
+  instance.size = stackMethods.size;
+  instance.pop = stackMethods.pop;
+  instance.push = stackMethods.push;
+  instance.total = 0;
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function(){
+  return this.total;
+};
 
+stackMethods.push = function(value){
+  this.total++;
+  this[this.total] = value;
+};
+stackMethods.pop = function(){
+  if (this.total > 0){
+    var temp = this[this.total];
+    delete this[this.total];
+    this.total--;
+  }
+  return temp;
+};
