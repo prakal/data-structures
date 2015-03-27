c13ba6b160a85d8ae5531c18caa51aadd778b90d
prakal <kalvapal@ualberta.ca>
2015-03-27 14:18:39 -0700

    passed all but one tests for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -24,14 +24,44 @@ treeMethods.addChild = function(value){
   this.children.push(newObj);
 };
 
-treeMethods.contains = function(target){
-  console.dir(this.children);
-  for (var i = 0; i < this.children.length; i++){
-    if (this.children[i].value === target) {
-      return true;
+treeMethods.contains = function(target, node){
+  // console.dir(this.children);
+  // for (var i = 0; i < this.children.length; i++){
+  //   if (this.children[i].value === target) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+  //
+  // using recursion to implement contains:
+  //
+  node = node || this.children[0];
+  var result;
+
+
+  // compares target with node.value
+  if (target === node.value) {
+    result = true;
+  }
+  // else{
+  //   result = false;
+  // }
+  // termination case
+  if (result) {
+    return result;
+  } else {
+    // Iterate trhoughout the children array
+    for (var i = 0; i < node.children.length; i++){
+      // grab the child object
+      var childObj = node.children[i];
+      // run the recursion on the child object
+      result =  this.contains(target, childObj);
     }
   }
-  return false;
+  if (!result){
+    return false;
+  }
+
 };
 
 
