0e575f492f107646abc86f15be03892c1fe7a309
prakal <kalvapal@ualberta.ca>
2015-03-27 21:34:58 -0700

    passes all tests in set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
+  console.dir(this._storage);
 };
 
 setPrototype.contains = function(item){
+  for (var key in this._storage){
+    console.log('key',this._storage[key]);
+    if (this._storage[key] === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
