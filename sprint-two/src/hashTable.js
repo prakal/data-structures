f3f08ed722ac0dec8ca9c28cbe10d7f354d28504
prakal <kalvapal@ualberta.ca>
2015-03-28 11:13:22 -0700

    passes extra credit of hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,8 +2,18 @@ var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
+HashTable.prototype.getStorageLength = function(){
+  return Object.keys(this._storage).length;
+};
 
 HashTable.prototype.insert = function(k, v){
+  // console.log('storageLength:',storageLength);
+  // Check if there's enough spots in the storage array
+  // if not, double the limit of the storage array.
+  if (this.getStorageLength() === this._limit){
+    this._limit *= 2;
+  }
+
   // invokes hashing function to provide i from array size and key
   var i = getIndexBelowMaxForKey(k, this._limit);
 
@@ -11,7 +21,7 @@ HashTable.prototype.insert = function(k, v){
   this._storage[i] = this._storage[i] || [];
   // push the 'tuple' array into the bucket array.
   this._storage[i].push([k,v]);
-  console.dir(this._storage[i]);
+  // console.dir(this._storage[i]);
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -29,6 +39,10 @@ HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   for (var j = 0; j < this._storage[i].length; j++){
     this._storage[i][j][1] = null;
+  }
+  console.log('key length',Object.keys(this._storage));
+  if (this.getStorageLength() < this._limit/2 + 3){
+    this._limit /= 2;
   }
 };
 
